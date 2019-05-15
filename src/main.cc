9f5899c3adf711bb71bd7ccf3e063675cb62c9b2
Claus-Justus Heine <himself@claus-justus-heine.de>
2019-05-15 08:26:40 +0200

    Add serial write.

--- src/main.cc
+++ src/main.cc
@@ -2,8 +2,10 @@
 
 int main(void)
 {
+  Serial.begin(9600);
   pinMode(13, OUTPUT);
   while (1) {
+    Serial.println("Teensy 3.6");
     digitalWriteFast(13, HIGH);
     delay(50);
     digitalWriteFast(13, LOW);
