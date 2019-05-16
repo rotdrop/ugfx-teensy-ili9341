3de27503a11dd2d01e02af92e3ace21005fdd43c
Claus-Justus Heine <himself@claus-justus-heine.de>
2019-05-16 21:52:59 +0200

    Test TWI.

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include <WProgram.h>
+#include <i2c_t3.h>
 
 #include "pins.hh"
 #include "ftm.hh"
@@ -67,7 +68,6 @@ using ServoController = CJHTeensy::ServoController<true>;
 using CJHTeensy::FTMModule;
 
 volatile uint32_t jiffies;
-volatile auto blah = FTMModule::instance(0).PWMLOAD;
 
 void ftm0_isr()
 {
@@ -97,7 +97,6 @@ void ftm0_isr()
       FTMModule::instance(i).CH[pin].CV = tick;
     }
     FTMModule::instance(i).PWMLOAD |= FTM_PWMLOAD_LDOK;
-    blah = FTMModule::instance(i).PWMLOAD;
   }
 }
 
@@ -105,27 +104,66 @@ ServoController servos;
 
 extern "C" volatile uint32_t systick_millis_count;
 
+// Memory
+#define MEM_LEN 256
+char databuf[MEM_LEN];
+int count;
+uint8_t target = 0x77; // target Slave address
+
 int main(void)
 {
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Setup for Master mode, pins 18/19, external pullups, 400kHz, 200ms default timeout
+  Wire.begin(I2C_MASTER, 0x00, I2C_PINS_18_19, I2C_PULLUP_EXT, 400000);
+  Wire.setDefaultTimeout(200000); // 200ms
+  
   while (1) {
     while (auto x = Serial.available()) {
       Serial.read();
       Serial.println("serial input");
       Serial.println(x, DEC);
     }
-    Serial.println("Teensy 3.6");
+    Serial.print("Teensy 3.6 @ ");
     Serial.println(jiffies, DEC);
-    Serial.println("OVF");
-    Serial.println(ServoController::overflow_, DEC);
-    Serial.println((ServoController::tick_ + ServoController::frequency_ - 1) / ServoController::frequency_, DEC);
-    Serial.println(FTMModule::instance(0).CH[0].CV, DEC);
-    Serial.println(FTMModule::instance(0).PWMLOAD, HEX);
-    Serial.println(blah, HEX);
-    digitalWriteFast(13, HIGH);
+
+    // Construct data message
+    sprintf(databuf, "Data Message #%d", count++);
+
+    // Print message
+    Serial.printf("Sending to Slave: '%s' ", databuf);
+        
+    // Transmit to Slave
+    Wire.beginTransmission(target);   // Slave address
+    Wire.write(databuf,strlen(databuf)+1); // Write string to I2C Tx buffer (incl. string null at end)
+    Wire.endTransmission();           // Transmit to Slave
+
+    // Check if error occured
+    if(Wire.getError()) {
+      Serial.println("FAIL\n");
+    } else {
+      Serial.println("OK\n");
+    }
+
+    // Print message
+    Serial.print("Reading from Slave: ");
+        
+    // Read from Slave
+    Wire.requestFrom(target, (size_t)MEM_LEN); // Read from Slave (string len unknown, request full buffer)
+    
+    // Check if error occured
+    if(Wire.getError()) {
+      Serial.print("FAIL\n");
+    } else {
+      // If no error then read Rx data into buffer and print
+      Wire.read(databuf, Wire.available());
+      Serial.printf("'%s' OK\n",databuf);
+    }
+    
+    digitalWriteFast(LED_BUILTIN, HIGH);
     delay(50);
-    digitalWriteFast(13, LOW);
-    delay(5000);
+    digitalWriteFast(LED_BUILTIN, LOW);
+    delay(4950);
   }
 }
