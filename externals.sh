d272c355c2129a62347ec85b1c3b437af30d3425
Claus-Justus Heine <himself@claus-justus-heine.de>
2019-07-14 14:21:28 +0200

    Work on ...

--- externals.sh
+++ externals.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
-while read PATH BRANCH REMOTE TYPE ; do
-    echo "$PATH;$BRANCH;$REMOTE;$TYPE"
+# DRY=echo
+
+top_srcdir=$(dirname $(realpath $0))
+
+cd $top_srcdir
+
+function createExternal ()
+{
+    local LOCALPATH=$1
+    local BRANCH=$2
+    local REMOTE=$3
+    local TYPE=$4
+
+    if [ "$TYPE" = subtree ]; then
+        $DRY git subtree add --prefix $LOCALPATH $REMOTE $BRANCH --squash
+    else
+        $DRY git submodule add -b $BRANCH $REMOTE $LOCALPATH
+    fi
+}
+
+while IFS=\; read LOCALPATH BRANCH REMOTE TYPE ; do
+    if [ "$LOCALPATH" = "LOCALPATH" ]; then
+        continue
+    fi
+    echo "$LOCALPATH;$BRANCH;$REMOTE;$TYPE"
+    if [ -d $top_srcdir/$LOCALPATH ]; then
+        echo $LOCALPATH exists
+    else
+        createExternal $LOCALPATH $BRANCH $REMOTE $TYPE
+    fi
 done < EXTERNALS.csv
